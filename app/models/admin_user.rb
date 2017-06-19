6dec746a0ca816db94105a94272f470db16f70b5
Tawsif Aqib <tawsif.aqib@gmail.com>
2017-06-19 22:27:41 +0600

    add active admin gem

---
+++ app/models/admin_user.rb
@@ -0,0 +1,9 @@
+class AdminUser < ApplicationRecord
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
+  			 # :confirmable, :lockable, :timeoutable, :omniauthable
+
+  def to_s
+  	"#{first_name} #{last_name}"
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base::establish_connection
+ActiveRecord::Base::connection.tables.each do |table|
+  next if table == 'schema_migrations'
+  ActiveRecord::Base::connection.execute('SET FOREIGN_KEY_CHECKS=0;')
+  ActiveRecord::Base::connection.execute("TRUNCATE #{table}")
+  ActiveRecord::Base::connection.execute('SET FOREIGN_KEY_CHECKS=1;')
+end
+
+AdminUser.create!(first_name: 					 'Admin',
+									email: 								 'admin@example.com',
+									password: 						 'password',
+									password_confirmation: 'password')
